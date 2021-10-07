d66bcdb83b1364f3ecd32ffce89983cbf6abbe76
Vitalii Pikozh <wisart@gmail.com>
2021-10-07 23:32:20 +0300

    add increment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import Widget from './components/Widget';
 import './App.css';
 
 function App() {
-  return (
-    
-  );
+  return <Widget>Who are you?</Widget>;
 }
 
 export default App;
--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -1,21 +1,46 @@
+import React from 'react';
 import s from './Widget.module.css';
+class Widget extends React.Component {
+  static defaultProps = {};
 
-function Widget = () => {
-  return (
-    
-    <div className={s.container}>
-      <p className={s.title}>Please leave feedback</p>
-      <div className={s.bottons}>
-        <button type="button">Good</button>
-        <button type="button">Neutral</button>
-        <button type="button">Bad</button>
-      </div>
-      <p className={s.title}>Statistics</p>
-      <p>Good:</p>
-      <p>Neutral:</p>
-      <p>Bad:</p>
-    </div>
-  )
+  static propTypes = {};
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = option => {
+    this.setState(prevStat => ({
+      [option]: prevStat[option] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h1>It's my life</h1>
+        <div className={s.container}>
+          <p className={s.title}>Please leave feedback</p>
+          <div className={s.bottons}>
+            <button onClick={() => this.handleIncrement('good')} type="button">
+              Good
+            </button>
+            <button onClick={() => this.handleIncrement('neutral')} type="button">
+              Neutral
+            </button>
+            <button onClick={() => this.handleIncrement('bad')} type="button">
+              Bad
+            </button>
+          </div>
+          <p className={s.title}>Statistics</p>
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+        </div>
+      </>
+    );
+  }
 }
-
 export default Widget;

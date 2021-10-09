5af2b3402a75d226943c8ec368d18bf22c79e411
Vitalii Pikozh <wisart@gmail.com>
2021-10-09 08:29:14 +0300

    add component Statistics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import Widget from './components/Widget';
-import './App.css';
+import React from 'react';
+import { Statistics } from './components/Widget/Statistics';
+class App extends React.Component {
+  static defaultProps = {};
 
-function App() {
-  return <Widget>Who are you?</Widget>;
+  static propTypes = {};
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = option => {
+    this.setState(prevStat => ({
+      [option]: prevStat[option] + 1,
+    }));
+  };
+
+  countTotalFeedbacks = () => this.state.good + this.state.neutral + this.state.bad;
+
+  countPositiveFeedbackPercentage = totalFeedbacks =>
+    Math.round((this.state.good / totalFeedbacks) * 100);
+  render() {
+    const { good, neutral, bad } = this.state;
+    const { handleIncrement, countTotalFeedbacks, countPositiveFeedbackPercentage } = this;
+    return (
+      <>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          handleIncrement={handleIncrement}
+          countTotalFeedbacks={countTotalFeedbacks}
+          countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+        />
+      </>
+    );
+  }
 }
-
 export default App;
